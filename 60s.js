6593d8bb4b7b36fcb5af7a53299e47bf7829d0d6
puestotv <47580301+puestotv@users.noreply.github.com>
2019-02-13 01:42:28 +0100

    Create 60s.js

---
+++ 60s.js
@@ -0,0 +1,12 @@
+
+module.exports.run = async (bot,message,args) => {
+
+    message.channel.send("Prochaine game dans 1 minute!");
+    
+ 
+ 
+ }
+ 
+ module.exports.help = {
+     name: "60.js"
+ }
